f2755cd8dad3d382291ffac605bd61ec3ac8a344
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-02-08 10:06:00 +0300

    adding properties

---
+++ app.js
@@ -0,0 +1,11 @@
+//items
+const menu=[
+    {
+        id:1,
+        title: "cocktails",
+        category: "drinks",
+        price: 15.99,
+        img: "mint.jpeg",
+        desc: `we offer a wide range of cocktails`,
+    },
+]
